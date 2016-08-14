dd8f3699a1e8fd6ca02fceaaa698155a2dffa761
insolita <webmaster100500@ya.ru>
2016-08-15 00:26:33 +0800

    reformat

--- gii/default_structure/mass.php
+++ gii/default_structure/mass.php
@@ -21,65 +21,69 @@ class <?= $migrationName ?> extends Migration
 
     public function init()
     {
-        $this->db = 'db';
+        $this->db = '<?=$generator->db?>';
         parent::init();
     }
 
     public function safeUp()
     {
         $tableOptions = '<?=$generator->tableOptions?>';
-$connection=Yii::$app-><?=$generator->db?>;
-$transaction=$connection->beginTransaction();
-try{
-<?php foreach($tableList as $tableData):?>
- $this->createTable('<?= ($generator->usePrefix)?$tableData['alias']:$tableData['name'] ?>',
-        [
-        <?php foreach($tableData['columns'] as $name=>$data):?>
-        '<?=$name?>'=> <?=$data;?>,
-        <?php endforeach;?>
-        ], $tableOptions);
-
-<?php if(!empty($tableData['indexes']) && is_array($tableData['indexes'])):?>
-    <?php foreach($tableData['indexes'] as $name=>$data):?>
-        <?php if($name!='PRIMARY'):?>
-  $this->createIndex('<?=$name?>', '<?=$tableData['alias']?>','<?=implode(",",array_values($data['cols']))?>',<?=$data['isuniq']?>);
-        <?php endif;?>
-    <?php endforeach;?>
- <?php endif?>
-<?php endforeach;?>
-<?php if(!empty($tableRelations) && is_array($tableRelations)):?>
-    <?php foreach($tableRelations as $table):?>
-        <?php foreach($table['fKeys'] as $i=>$rel):?>
-  $this->addForeignKey('fk_<?=$table['tableName']?>_<?=$rel['pk']?>', '<?=$table['tableAlias']?>', '<?=$rel['pk']?>', '<?=$rel['ftable']?>', '<?=$rel['fk']?>');
-        <?php endforeach;?>
-    <?php endforeach;?>
-<?php endif?>
-$transaction->commit();
-} catch (Exception $e) {
-echo 'Catch Exception '.$e->getMessage().' and rollBack this';
-    $transaction->rollBack();
-}
+        $transaction=$this->db->beginTransaction();
+        try{
+            <?php foreach ($tableList as $tableData) :?>
+            $this->createTable('<?= ($generator->usePrefix)?$tableData['alias']:$tableData['name'] ?>',
+               [
+                    <?php foreach ($tableData['columns'] as $name => $data) :?>
+                    '<?=$name?>'=> <?=$data;?>,
+                    <?php endforeach;?>
+               ], $tableOptions);
+                <?php if (!empty($tableData['indexes']) && is_array($tableData['indexes'])) :?>
+                    <?php foreach ($tableData['indexes'] as $name => $data) :?>
+                            <?php if ($name!='PRIMARY') :?>
+            $this->createIndex('<?=$name?>',
+                            '<?=$tableData['alias']?>',
+                            '<?=implode(",", array_values($data['cols']))?>',
+                            <?=$data['isuniq']?>);
+                            <?php endif;?>
+                    <?php endforeach;?>
+                <?php endif?>
+            <?php endforeach;?>
+            <?php if (!empty($tableRelations) && is_array($tableRelations)) :?>
+                <?php foreach ($tableRelations as $table) :?>
+                    <?php foreach ($table['fKeys'] as $i => $rel) :?>
+            $this->addForeignKey('fk_<?=$table['tableName']?>_<?=$rel['pk']?>',
+                        '<?=$table['tableAlias']?>',
+                        '<?=$rel['pk']?>',
+                        '<?=$rel['ftable']?>',
+                        '<?=$rel['fk']?>');
+                    <?php endforeach;?>
+                <?php endforeach;?>
+            <?php endif?>
+            $transaction->commit();
+        } catch (Exception $e) {
+             echo 'Catch Exception '.$e->getMessage().' and rollBack this';
+             $transaction->rollBack();
+        }
     }
 
     public function safeDown()
     {
-$connection=Yii::$app-><?=$generator->db?>;
-$transaction=$connection->beginTransaction();
-try{
-<?php if(!empty($tableRelations) && is_array($tableRelations)):?>
-    <?php foreach($tableRelations as $table):?>
-        <?php foreach($table['fKeys'] as $i=>$rel):?>
+        $transaction=$this->db->beginTransaction();
+        try{
+            <?php if (!empty($tableRelations) && is_array($tableRelations)) :?>
+                <?php foreach ($tableRelations as $table) :?>
+                    <?php foreach ($table['fKeys'] as $i => $rel) :?>
             $this->dropForeignKey('fk_<?=$table['tableName']?>_<?=$rel['pk']?>', '<?=$table['tableAlias']?>');
-        <?php endforeach;?>
-    <?php endforeach;?>
-<?php endif?>
-<?php foreach($tableList as $tableData):?>
-    $this->dropTable('<?= ($generator->usePrefix)?$tableData['alias']:$tableData['name']?>');
-<?php endforeach;?>
-$transaction->commit();
-} catch (Exception $e) {
-echo 'Catch Exception '.$e->getMessage().' and rollBack this';
-$transaction->rollBack();
-}
+                    <?php endforeach;?>
+                <?php endforeach;?>
+            <?php endif?>
+            <?php foreach ($tableList as $tableData) :?>
+            $this->dropTable('<?= ($generator->usePrefix)?$tableData['alias']:$tableData['name']?>');
+            <?php endforeach;?>
+            $transaction->commit();
+        } catch (Exception $e) {
+            echo 'Catch Exception '.$e->getMessage().' and rollBack this';
+            $transaction->rollBack();
+        }
     }
 }
---
+++ resolver/TableResolver.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by solly [14.08.16 23:02]
+ */
+
+namespace insolita\migrik\resolver;
+
+
+class TableResolver
+{
+
+}
