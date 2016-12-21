a678e1dc5895496a53af42ede116fba593323e60
insolita <webmaster100500@ya.ru>
2016-12-22 04:38:22 +0800

    fix #22 missing comma

--- gii/default_structure/mass.php
+++ gii/default_structure/mass.php
@@ -47,7 +47,7 @@ class <?= $migrationName ?> extends Migration
 <?php if (!empty($tableRelations) && is_array($tableRelations)) :?>
 <?php foreach ($tableRelations as $table) :?>
 <?php foreach ($table['fKeys'] as $i => $rel) :?>
-            $this->addForeignKey('fk_<?=$table['tableName']?>_<?=$rel['pk']?>','<?=$table['tableAlias']?>','<?=$rel['pk']?>','<?=$rel['ftable']?>''<?=$rel['fk']?>');
+            $this->addForeignKey('fk_<?=$table['tableName']?>_<?=$rel['pk']?>','<?=$table['tableAlias']?>','<?=$rel['pk']?>','<?=$rel['ftable']?>','<?=$rel['fk']?>');
 <?php endforeach;?>
 <?php endforeach;?>
 <?php endif?>
