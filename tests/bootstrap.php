dcf96a5a23c5c455fb4ac7fe3dc59d2108cc7b17
insolita <webmaster100500@ya.ru>
2017-04-29 08:46:58 +0800

    upd

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,6 +5,12 @@ defined('YII_ENV') or define('YII_ENV', 'test');
 
 
 require_once(__DIR__ . '/../../../../vendor/autoload.php');
+/**
+ * Load application environment from .env file
+ */
+$dotenv = new \Dotenv\Dotenv(__DIR__);
+$dotenv->load();
 require_once(__DIR__ . '/../../../../vendor/yiisoft/yii2/Yii.php');
+
 
 \Yii::setAlias('@tests', __DIR__);
